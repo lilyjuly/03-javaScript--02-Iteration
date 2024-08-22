19a2432b142a9a51bef7c3c4a092d4a60a7d197b
lily <lilyjulymm2009@gmail.com>
2024-08-22 19:11:13 +0630

    02-Iteration

---
+++ script.js
@@ -0,0 +1,5 @@
+var students = ['Sarah', 'Orlando', 'Ismael', 'Hung'];
+console.log(students.length);
+for (var i = 0; i < students.length; i++) {
+    console.log('Great to see you, ' + students[i] + '!');
+}
